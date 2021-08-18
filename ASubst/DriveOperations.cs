6279e48f7d4755f8c0c4fc87623ebd5982708e98
Adrian Sims <adriansims62781@gmail.com>
2021-08-18 19:37:43 +0200

    Add project files.

---
+++ ASubst/DriveOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ASubst
+{
+    static class DriveOperations
+    {
+        [DllImport("kernel32.dll")]
+        static extern bool DefineDosDevice(uint dwFlags, string lpDeviceName, string lpTargetPath);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern uint QueryDosDevice(string lpDeviceName, StringBuilder lpTargetPath, int ucchMax);
+
+        public static bool UnmapDrive(char driveLetter)
+        {
+            return DefineDosDevice(2, $"{driveLetter}:", null);
+        }
+
+        public static string GetDriveMapping(char driveLetter)
+        {
+            var sb = new StringBuilder();
+            QueryDosDevice($"{driveLetter}:", sb, 65536);
+            var str = sb.ToString();
+            if (str.StartsWith("\\??\\"))
+            {
+                return str.Substring(4).Trim();
+            }
+            return str.Trim();
+        }
+
+        public static void MapDrive(char driveLetter, string dir)
+        {
+            DefineDosDevice(0, $"{driveLetter}:", dir);
+        }
+    }
+}
---
+++ ASubst/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace ASubst
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var optionsArgs = args.Where(x => x[0] == '-').SelectMany(x => x.Skip(1)).ToHashSet();
+                var normalArgs = args.Where(x => x[0] != '-').ToList();
+
+                var haveDrive = normalArgs.Any() && normalArgs.First().Length == 1 && char.IsLetter(normalArgs[0][0]);
+                var drive = haveDrive ? normalArgs[0][0] : '-';
+
+                if (normalArgs.Count == 2 && haveDrive)
+                {
+                    // unmap existing drive - don't care if we fail:
+                    DriveOperations.UnmapDrive(drive);
+
+                    // map new drive:
+                    DriveOperations.MapDrive(drive, normalArgs[1]);
+                }
+                else if (optionsArgs.Contains('d') && haveDrive && normalArgs.Count == 1)
+                {
+                    if (!DriveOperations.UnmapDrive(drive))
+                    {
+                        Console.Error.WriteLine($"drive {drive} is not mapped.");
+                    }
+                }
+                else if (optionsArgs.Contains('t') && haveDrive && normalArgs.Count == 1)
+                {
+                    var dir = DriveOperations.GetDriveMapping(drive);
+                    if (!string.IsNullOrEmpty(dir))
+                    {
+                        Console.WriteLine(dir);
+                    }
+                }
+                else if (normalArgs.Count == 1 && haveDrive)
+                {
+                    DriveOperations.UnmapDrive(drive);
+
+                    // map new drive to current directory:
+                    DriveOperations.MapDrive(drive, Directory.GetCurrentDirectory());
+                }
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+        }
+    }
+}
